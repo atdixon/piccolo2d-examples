a46cf92e71dcf0a25e97dc942383869866329358
Aaron Dixon <atdixon@gmail.com>
2010-10-10 01:48:59 -0500

    strategies

--- src/main/java/atdixon/piccolo/example/LineBendExample.java
+++ src/main/java/atdixon/piccolo/example/LineBendExample.java
@@ -20,13 +20,17 @@ public class LineBendExample extends PFrame {
     enum Bend { LEFT, RIGHT, ANY }
 
     private static final int DIAM = 25;
+    private static final int KDIAM = DIAM + 3;
     private static final int STRESS = 200;
     private static final int QUANTUM = 20;
-    private static final int MAX_BEND = 160; // in degrees
+    private static final int MAX_BEND = 180; // in degrees
+    private static final int STATIC_BEND = 90;
 
     public static void main(String[] args) {
         new LineBendExample();
     }
+
+    private Strategy strategy = new RotationStrategy();
 
     private PPath line, c1, c2, km, k1, k2;
 
@@ -45,16 +49,16 @@ public class LineBendExample extends PFrame {
         c1 = PPath.createEllipse(0, 0, DIAM, DIAM);
         c2 = PPath.createEllipse(0, 0, DIAM, DIAM);
 
-        km = PPath.createEllipse(0, 0, DIAM, DIAM);
+        km = PPath.createEllipse(0, 0, KDIAM, KDIAM);
         km.setStrokePaint(Color.red);
-        k1 = PPath.createEllipse(0, 0, DIAM, DIAM);
+        k1 = PPath.createEllipse(0, 0, KDIAM, KDIAM);
         k1.setStrokePaint(Color.blue);
-        k2 = PPath.createEllipse(0, 0, DIAM, DIAM);
+        k2 = PPath.createEllipse(0, 0, KDIAM, KDIAM);
         k2.setStrokePaint(Color.blue);
         
         PropertyChangeListener l = new PropertyChangeListener() {
             public void propertyChange(PropertyChangeEvent evt) {
-                rebuildCurve();
+                strategy.rebuildCurve();
             }
         };
 
@@ -65,12 +69,13 @@ public class LineBendExample extends PFrame {
         c2.addPropertyChangeListener(PNode.PROPERTY_FULL_BOUNDS, l);
 
         layer.addChild(line);
-        layer.addChild(c1);
-        layer.addChild(c2);
 
         layer.addChild(km);
         layer.addChild(k1);
         layer.addChild(k2);
+
+        layer.addChild(c1);
+        layer.addChild(c2);
 
         // add some helpful labels
         c1.addChild(text("c1"));
@@ -97,52 +102,6 @@ public class LineBendExample extends PFrame {
 
         c1.addInputEventListener(drag);
         c2.addInputEventListener(drag);
-    }
-
-    private void rebuildCurve() {
-        Point2D p1 = c1.getFullBounds().getCenter2D();
-        Point2D p2 = c2.getFullBounds().getCenter2D();
-
-        double dist = dist(p1, p2);
-
-        if (dist > STRESS) {
-            bend = Bend.ANY;
-        } else if (bend == Bend.ANY) {
-            bend = p1.getY() < p2.getY() ? Bend.LEFT : Bend.RIGHT;
-        }
-
-        Point2D mid = midpoint(p1, p2);
-
-        // relative mids
-        Point2D mc1 = new Point2D.Double(mid.getX() - p1.getX(), mid.getY() - p1.getY());
-        Point2D mc2 = new Point2D.Double(mid.getX() - p2.getX(), mid.getY() - p2.getY());
-
-        Point2D c1 = makeDistanceFromOrigin(mc1, STRESS / 2);
-        Point2D c2 = makeDistanceFromOrigin(mc2, STRESS / 2);
-
-        double t = dist > STRESS ? 1 : (dist / STRESS);
-
-        Point2D rc1 = rotate(c1, (1 - t) * Math.toRadians(bend == Bend.RIGHT ? -MAX_BEND : MAX_BEND));
-        Point2D rc2 = rotate(c2, (1 - t) * Math.toRadians(bend == Bend.RIGHT ? MAX_BEND : -MAX_BEND));
-
-        c1 = translate(rc1, p1);
-        c2 = translate(rc2, p2);
-
-        k1.centerFullBoundsOnPoint(c1.getX(), c1.getY());
-        k2.centerFullBoundsOnPoint(c2.getX(), c2.getY());
-
-        km.centerFullBoundsOnPoint(mid.getX(), mid.getY());
-
-        float x3 = (float) c1.getX();
-        float y3 = (float) c1.getY();
-
-        float x4 = (float) c2.getX();
-        float y4 = (float) c2.getY();
-
-        line.reset();
-        line.moveTo((float) p1.getX(), (float) p1.getY());
-        line.curveTo(x3, y3, x4, y4, (float) p2.getX(), (float) p2.getY());
-        line.lineTo((float) p2.getX(), (float) p2.getY());
     }
 
     private void repel(PNode moveable, PNode still) {
@@ -198,6 +157,93 @@ public class LineBendExample extends PFrame {
         text.setPickable(false);
         return text;
     }
-    
+
+    /** Strategy. */
+    abstract class Strategy {
+
+        abstract double controlPointRotation(double t);
+        abstract double controlPointDistance(double t);
+
+        void rebuildCurve() {
+            Point2D p1 = c1.getFullBounds().getCenter2D();
+            Point2D p2 = c2.getFullBounds().getCenter2D();
+
+            double dist = dist(p1, p2);
+
+            if (dist > STRESS) {
+                bend = Bend.ANY;
+            } else if (bend == Bend.ANY) {
+                bend = p1.getY() < p2.getY() ? Bend.LEFT : Bend.RIGHT;
+            }
+
+            Point2D mid = midpoint(p1, p2);
+
+            // relative mids
+            Point2D mc1 = new Point2D.Double(mid.getX() - p1.getX(), mid.getY() - p1.getY());
+            Point2D mc2 = new Point2D.Double(mid.getX() - p2.getX(), mid.getY() - p2.getY());
+
+            double t = dist > STRESS ? 1 : (dist / STRESS);
+
+            double cpd = controlPointDistance(t);
+
+            Point2D c1 = makeDistanceFromOrigin(mc1, cpd);
+            Point2D c2 = makeDistanceFromOrigin(mc2, controlPointDistance(t));
+
+            double cpr = controlPointRotation(t);
+            
+            Point2D rc1 = rotate(c1, Math.toRadians(bend == Bend.RIGHT ? -cpr : cpr));
+            Point2D rc2 = rotate(c2, Math.toRadians(bend == Bend.RIGHT ? cpr : -cpr));
+
+            c1 = translate(rc1, p1);
+            c2 = translate(rc2, p2);
+
+            k1.centerFullBoundsOnPoint(c1.getX(), c1.getY());
+            k2.centerFullBoundsOnPoint(c2.getX(), c2.getY());
+
+            km.centerFullBoundsOnPoint(mid.getX(), mid.getY());
+
+            float x3 = (float) c1.getX();
+            float y3 = (float) c1.getY();
+
+            float x4 = (float) c2.getX();
+            float y4 = (float) c2.getY();
+
+            line.reset();
+            line.moveTo((float) p1.getX(), (float) p1.getY());
+            line.curveTo(x3, y3, x4, y4, (float) p2.getX(), (float) p2.getY());
+            line.lineTo((float) p2.getX(), (float) p2.getY());
+        }
+
+    }
+
+    /** Orthogonal. */
+    private final class OrthogonalStrategy extends Strategy {
+
+        @Override
+        double controlPointRotation(double t) {
+            return t > 1 ? 0 : STATIC_BEND;
+        }
+
+        @Override
+        double controlPointDistance(double t) {
+            return (1 - t) * STRESS / 2;
+        }
+
+    }
+
+    /** Rotation. */
+    private final class RotationStrategy extends Strategy {
+
+        @Override
+        double controlPointRotation(double t) {
+            return (1 - t) * MAX_BEND;
+        }
+
+        @Override
+        double controlPointDistance(double t) {
+            return STRESS / 2;
+        }
+
+    }
 
 }
